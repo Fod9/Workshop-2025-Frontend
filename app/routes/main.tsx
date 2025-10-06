c591987b59aa8bf9f9f8d341aff46c133998446e
Fod9 <dernoncourt.tom@gmail.com>
2025-10-06 15:55:05 +0200

    feat: début home page

---
+++ app/routes/main.tsx
@@ -0,0 +1,11 @@
+import "../styles/main.css"
+
+export default function Home() {
+  return (
+    <div className="home-page-container">
+      <h1>EcoRescue</h1>
+      <button className="join-game-button">Rejoindre une partie</button>
+      <button className="create-game-button">Créer une partie</button>
+    </div>
+  )
+}
